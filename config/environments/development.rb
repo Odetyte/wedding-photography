0f07700e7a7155473aee9e685f8bd18228f8ec7e
Odeta Maslovaite <odetamaslovaite@yahoo.com>
2020-10-30 20:59:28 +0100

    changing port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
-    port:                 587,
+    port:                 25,
     domain:               'wed-ph.herokuapp.com',
     user_name:            ENV["GMAIL_EMAIL"],
     password:             ENV["GMAIL_PASSWORD"],
