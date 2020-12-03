12d60c9bcc67130abcd5151d3025303555152f5c
Odeta Maslovaite <odetamaslovaite@yahoo.com>
2020-12-03 21:17:41 +0100

    some adjustments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -14,7 +14,7 @@ class PagesController < ApplicationController
         format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
       else
         format.html { render 'index' }
-        format.js   { flash.now[:error] = @message = "Message did not send." }
+        format.js   { flash.now[:error] = @message = "Message did not send" }
       end
     end
   end
