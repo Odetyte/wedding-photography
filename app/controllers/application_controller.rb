e9cbe0fb222f0263dfa237c6c76e1b9017fcf42c
Odetyte <odetamaslovaite@yahoo.com>
2022-02-07 14:52:56 +0100

    fix gramma & layout throughout the page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: true
+  add_flash_types :success, :error
+  # skip_before_action :verify_authenticity_token
+  # protect_from_forgery prepend: true
+  # protect_from_forgery prepend: true
+  protect_from_forgery with: :null_session
   def default_url_options
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
